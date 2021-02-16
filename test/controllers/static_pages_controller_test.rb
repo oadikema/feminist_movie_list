deee4904a38cb5e72eeafa084429a19bf2fccc21
Obuchi Adikema <obuchiadikema@gmail.com>
2021-02-16 02:16:36 +0000

    modified Minitests - HW5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Obuchi's Individual Project"
+  end
 
     test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
+  
+  test "should get roll" do
+    get static_pages_roll_url
+    assert_response :success
+    assert_select "title", "Roll | #{@base_title}"
+  end
+  
 end
