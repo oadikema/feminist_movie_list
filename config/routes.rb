5f0b4754df255f781bf9d633c2b7782c627c398e
Obuchi Adikema <obuchiadikema@gmail.com>
2021-02-21 03:21:21 +0000

    Fixed Nav Bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/roll'
+  get  '/home', to: 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/about', to: 'static_pages#about'
+  get  '/roll', to: 'static_pages#roll'
 end
 
 
