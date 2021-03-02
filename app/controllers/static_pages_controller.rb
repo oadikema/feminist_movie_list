94a8da03e5f442ddf7d267d733c45a624faf2717
Obuchi Adikema <obuchiadikema@gmail.com>
2021-03-02 03:56:15 +0000

    added random movie generator

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    movie_list = Movie.all
+
+    movieNum = (0..movie_list.length-1).to_a.shuffle.first
+
+    @movie = movie_list[movieNum]
   end
 
   def help
